8b019e275df8ea0df2cda65b0984b8d6b7d21420
Lucas Monteiro Vendruscolo <lucas.vendruscolo@gmail.com>
2022-11-06 16:16:30 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,148 @@
+//trocar imagem do botão de like e modifcar contador//
+function changeImage(){
+    let display = document.getElementById("like1");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes1").innerHTML = "101 likes"; ;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes1").innerHTML = "100 likes";;
+    }
+
+}
+
+function changeImage2(){
+    let display = document.getElementById("like2");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes2").innerHTML = "298 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes2").innerHTML = "297 likes";;
+    }
+}
+
+function changeImage3(){
+    let display = document.getElementById("like3");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes3").innerHTML = "405 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes3").innerHTML = "404 likes";;
+    }
+}
+
+function changeImage4(){
+    let display = document.getElementById("like4");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes4").innerHTML = "365 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes4").innerHTML = "364 likes";;
+    }
+}
+
+function changeImage5(){
+    let display = document.getElementById("like5");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes5").innerHTML = "968 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes5").innerHTML = "967 likes";;
+    }
+}
+
+function changeImage6(){
+    let display = document.getElementById("like6");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes6").innerHTML = "733 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes6").innerHTML = "732 likes";;
+    }
+}
+
+function changeImage7(){
+    let display = document.getElementById("like7");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes7").innerHTML = "1002 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes7").innerHTML = "1001 likes";;
+    }
+}
+
+function changeImage8(){
+    let display = document.getElementById("like8");
+    if(display.src.match("img/like.PNG")){
+        display.src = "img/liked.png", document.getElementById("likes8").innerHTML = "1315 likes";;
+    }else{
+        display.src = "img/like.PNG", document.getElementById("likes8").innerHTML = "1314 likes";;
+    }
+}
+// trocar imagem do botão de save//
+function changeSave(){
+    let display = document.getElementById("save1");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave2(){
+    let display = document.getElementById("save2");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave3(){
+    let display = document.getElementById("save3");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave4(){
+    let display = document.getElementById("save4");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave5(){
+    let display = document.getElementById("save5");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave6(){
+    let display = document.getElementById("save6");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave7(){
+    let display = document.getElementById("save7");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+function changeSave8(){
+    let display = document.getElementById("save8");
+    if(display.src.match("img/save.PNG")){
+        display.src = "img/saved.png";
+    }else{
+        display.src = "img/save.PNG";
+    }
+
+}
+
+//comentários//
